11a4e491a2e15829561e05bc68feb89a134d2918
David Stotijn <dstotijn@gmail.com>
2022-07-14 12:15:42 +0200

    Add "Download PDF" button

--- src/builder/Actions.svelte
+++ src/builder/Actions.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
   import utf8 from "utf8";
   import base64 from "base-64";
+  import classNames from "classnames";
+  import fileSaver from "file-saver";
 
   import * as GOBL from "../lib/gobl";
   import { keypair, editor, envelope, goblError, GOBLError } from "./stores";
   import Button from "../ui/Button.svelte";
   import { createNotification, Severity } from "./notifications";
+
+  const pdfApiBaseUrl = import.meta.env.VITE_PDF_API_BASE_URL;
 
   let buildable = false;
 
@@ -19,6 +23,7 @@
   });
 
   $: buildEnabled = Boolean($keypair) && buildable;
+  $: envelopeExists = Boolean($envelope);
 
   function encodeUTF8ToBase64(value: string): string {
     return base64.encode(utf8.encode(value));
@@ -82,20 +87,87 @@
       goblError.set(parseGOBLError(e));
     }
   }
+
+  async function handleDownloadPDFClick() {
+    const formData = new FormData();
+    formData.append("envelope", new Blob([JSON.stringify($envelope)]));
+
+    try {
+      const res = await fetch(`${pdfApiBaseUrl}/api`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!res.ok) {
+        createNotification({
+          severity: Severity.Error,
+          message: "The PDF service returned an error:",
+          context: `${await res.text()} (HTTP status: ${res.status})`,
+        });
+        return;
+      }
+
+      const filename = res.headers
+        .get("content-disposition")
+        .split(";")
+        .find((n) => n.includes("filename="))
+        .replace("filename=", "")
+        .trim();
+      // TODO: Make filename meaningful.
+      fileSaver.saveAs(await res.blob(), filename);
+
+      createNotification({
+        severity: Severity.Success,
+        message: "Downloaded PDF file.",
+      });
+    } catch (e) {
+      createNotification({
+        severity: Severity.Error,
+        message: "Failed to download PDF.",
+        context: e,
+      });
+    }
+  }
 </script>
 
 <div class="flex gap-2">
-  <div
-    id="tooltip-build"
-    role="tooltip"
-    class="inline-block absolute invisible z-10 py-1 px-2 text-xs text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
-  >
-    {#if buildEnabled}
-      Build the GOBL document.
-    {:else}
-      To build, first ensure the document is valid.
-    {/if}
-    <div class="tooltip-arrow" data-popper-arrow />
+  <div>
+    <div
+      id="tooltip-download-pdf"
+      role="tooltip"
+      class="inline-block absolute invisible z-10 py-1 px-2 text-xs text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+    >
+      Download PDF of last built document.
+      <div class="tooltip-arrow" data-popper-arrow />
+    </div>
+    <button
+      data-tooltip-target="tooltip-download-pdf"
+      class={classNames("p-2 hover:bg-gray-200 rounded-lg", {
+        hidden: !envelopeExists,
+      })}
+      on:click={handleDownloadPDFClick}
+      ><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path
+          fill-rule="evenodd"
+          d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        />
+      </svg></button
+    >
   </div>
-  <Button on:click={handleBuildClick} data-tooltip-target="tooltip-build" disabled={!buildEnabled}>Build</Button>
+  <div>
+    <div
+      id="tooltip-build"
+      role="tooltip"
+      class="inline-block absolute invisible z-10 py-1 px-2 text-xs text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+    >
+      {#if buildEnabled}
+        Build the GOBL document.
+      {:else}
+        To build, first ensure the document is valid.
+      {/if}
+      <div class="tooltip-arrow" data-popper-arrow />
+    </div>
+    <Button on:click={handleBuildClick} data-tooltip-target="tooltip-build" disabled={!buildEnabled}>Build</Button>
+  </div>
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    // TODO: Remove proxy once CORS header has been added at pdf.invopop.com/api.
+    proxy: {
+      "/pdf": {
+        target: "https://pdf.invopop.com",
+        secure: false,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/pdf/, ""),
+      },
+    },
+  },
 });
