c945b5b2c55e9adac0241ef041abe8c061dc625e
amalcaraz <amalcaraz89@gmail.com>
2023-05-03 21:58:09 +0200

    removed undo / redo action buttons in header

--- src/lib/menubar/ButtonsToolbar.svelte
+++ src/lib/menubar/ButtonsToolbar.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import Undo from "$lib/actions/Undo.svelte";
-  import Redo from "$lib/actions/Redo.svelte";
   import Build from "$lib/actions/Build.svelte";
   import ActionsButton from "./ActionsButton.svelte";
 
@@ -9,12 +7,6 @@
 </script>
 
 <div class="flex items-center justify-end py-1 text-xs font-medium">
-  <div class="flex items-center border-r-2 h-6 pr-4 mr-4">
-    <div class="flex items-center">
-      <Undo on:undo />
-      <Redo on:redo />
-    </div>
-  </div>
   <div class="flex items-center border-r-2 h-6 pr-4 mr-4 relative">
     <ActionsButton {jsonSchemaURL} {signEnabled} />
   </div>
