05eafd6369ce87df5b90701a28ebbbf3d6b12eb3
Javier Martinez <javier.martinez@invopop.com>
2024-01-22 11:46:56 +0000

    wrap signatures on a multiline div

--- src/lib/menubar/EnvelopeSignatures.svelte
+++ src/lib/menubar/EnvelopeSignatures.svelte
@@ -4,5 +4,17 @@
 </script>
 
 {#if sigs}
-  <pre class="text-sm text-neutral-800 bg-neutral-50 border border-neutral-100">{JSON.stringify(sigs, null, 4)}</pre>
+  <div
+    role="presentation"
+    contenteditable
+    on:keydown={(event) => {
+      // Allow copy
+      if (!event.metaKey) {
+        event.preventDefault();
+      }
+    }}
+    class="text-sm text-neutral-800 bg-neutral-50 border border-neutral-100 outline-none"
+  >
+    {JSON.stringify(sigs, null, 4)}
+  </div>
 {/if}
